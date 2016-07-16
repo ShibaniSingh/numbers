225064f2d36184994e6816ef0ede40d6125b2ee5
ShibaniSingh <shibani888ksriet@gmail.com>
2016-07-16 12:30:36 +0530

    Add files via upload

---
+++ LeapYeaar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leapyear;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class LeapYeaar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Enter the year:");
+        Scanner sc=new Scanner(System.in);
+        int year=sc.nextInt();
+        if(year%4==0)
+        {
+            System.out.println(year+" is a leap year");
+        }
+        else
+        {
+          System.out.println(year+" is not a leap year");  
+        }
+    }
+}
